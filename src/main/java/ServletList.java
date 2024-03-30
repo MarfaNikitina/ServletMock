647a8c206ff80b5044645fedb50a12897b973dc4
MarfaNikitina <marfik93@mail.ru>
2024-03-30 09:36:00 +0300

    Правки 3

--- src/main/java/ServletList.java
+++ src/main/java/ServletList.java
@@ -1,14 +1,11 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import logic.Model;
 import logic.User;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
@@ -17,63 +14,24 @@ import java.util.Map;
 public class ServletList extends HttpServlet {
     Model model = Model.getInstance();
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        response.setContentType("text/html;charset=utf-8");
-//        PrintWriter pw = response.getWriter();
-//        int id = Integer.parseInt(request.getParameter("id"));
-//        if(id == 0){
-//            pw.print("<html" + "<h3> Доступные Пользователи: </h3><br/>" +
-//                    "ID пользователя " +
-//                    "<ul>");
-//            for (Map.Entry<Integer, User> entry: model.getFromList().entrySet()) {
-//                pw.print("<li>" + entry.getKey() + "</li>" +
-//                        "<li>Имя: " + entry.getValue().getName() + "</li>" +
-//                        "<li>Фамилия: " + entry.getValue().getSurname() + "</li>" +
-//                        "<li>Зарплата: " + entry.getValue().getSalary() + "</li>" +
-//                        "</ul>");
-//            }
-//            pw.print("</ul>" +
-//                    "<a href=\"index.jsp\">Домой</a>" +
-//                    "</html");
-//        } else if (id > 0) {
-//            if (id > model.getFromList().size()) {
-//                pw.print("<html>" + "<h3> No such User </h3>" +
-//                        "<a href=\"index.jsp\">Домой</a>" +
-//                        "</html");
-//            } else {
-//                pw.print("<html>" + "<h3> Запршенный пользователь </h3>" +
-//                        "Имя: " + model.getFromList().get(id).getName() + "<br/>" +
-//                        "Фамилия: " + model.getFromList().get(id).getSurname() + "<br/>" +
-//                        "Зарплата: " + model.getFromList().get(id).getSalary() + "<br/>" +
-//                        "<a href=\"index.jsp\">Домой</a>" +
-//                        "</html");
-//            }
-//            } else {
-//            pw.print("<html>" + "<h3> ID должен быть больше 0 </h3>" +
-//                    "<a href=\"index.jsp\">Домой</a>" +
-//                    "</html");
-//
-//        }
-//
-//    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         request.setCharacterEncoding("UTF-8");
         response.setContentType("application/json;charset=utf-8");
-//        BufferedReader reader = request.getReader();
         PrintWriter pw = response.getWriter();
         int input = Integer.parseInt(request.getParameter("id"));
-        if (0 == input) {
-            pw.print(gson.toJson(model.getFromList()));
-        } else if (input > model.getFromList().size()) {
-            pw.print("No such user with ID" + request.getParameter("id"));
-        } else if (input < 0) {
-            pw.print("ID должно быть положительным числом");
+        if (input <= 0) {
+            pw.print("{\"message:\" \"ID должно быть положительным числом\"}");
         } else {
-            User user = model.getUser(input);
-            pw.print(gson.toJson(user));
-        }
+            for (Map.Entry<Integer, User> pair : model.getFromList().entrySet()) {
+                if (input == pair.getKey()) {
 
+                    request.setCharacterEncoding("UTF-8");
+                    User user = model.getUser(input);
+                    pw.print(gson.toJson(user));
+                    return;
+                }
+            }
+            pw.print("{\"message:\" \"No such user with ID:" + request.getParameter("id") + "\"}");
+        }
     }
 }
--- src/main/java/ServletPut.java
+++ src/main/java/ServletPut.java
@@ -1,6 +1,7 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import logic.Model;
 import logic.User;
 
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
 
 
 /*
@@ -38,29 +41,54 @@ public class ServletPut extends HttpServlet {
         } catch (Exception e) {
             System.out.println("Error");
         }
-
         response.setContentType("application/json;charset=utf-8");
         PrintWriter pw = response.getWriter();
-        int id = Integer.parseInt(request.getParameter("id"));
-        if (id > model.getFromList().size()) {
-            pw.print("{\"message:\" \"No such user with ID:" + request.getParameter("id") + "\"}");
-        } else if (id <= 0) {
-            pw.print("{\"message:\" \"ID должно быть положительным числом\"}");
-        } else {
-            JsonObject jobj = gson.fromJson(String.valueOf(jb), JsonObject.class);
+//        int id = Integer.parseInt(request.getParameter("id"));
+//        if (id > model.getFromList().size()) {
+//            pw.print("{\"message:\" \"No such user with ID:" + request.getParameter("id") + "\"}");
+//        } else if (id <= 0) {
+//            pw.print("{\"message:\" \"ID должно быть положительным числом\"}");
+//        } else {
+//            JsonObject jobj = gson.fromJson(String.valueOf(jb), JsonObject.class);
+//
+//            request.setCharacterEncoding("UTF-8");
+//
+//            String name = jobj.get("name").getAsString();
+//            String surname = jobj.get("surname").getAsString();
+//            double salary = jobj.get("salary").getAsDouble();
+//
+//            User user = new User(name, surname, salary);
+//            model.put(id, user);
+//
+//            pw.print("{\"message:\" \"Пользователь успешно изменен\"}");
+//
+//            pw.print(gson.toJson(model.getFromList()));
 
-            request.setCharacterEncoding("UTF-8");
+        try {
+            int input = Integer.parseInt(request.getParameter("id"));
+            if (input <= 0) {
+                pw.print("{\"message:\" \"ID должно быть положительным числом\"}");
+            } else {
+                for (Map.Entry<Integer, User> pair : model.getFromList().entrySet()) {
+                    if (input == pair.getKey()) {
+                        JsonObject jobj = gson.fromJson(String.valueOf(jb), JsonObject.class);
 
-            String name = jobj.get("name").getAsString();
-            String surname = jobj.get("surname").getAsString();
-            double salary = jobj.get("salary").getAsDouble();
+                        request.setCharacterEncoding("UTF-8");
 
-            User user = new User(name, surname, salary);
-            model.put(id, user);
-
-            pw.print("{\"message:\" \"Пользователь успешно изменен\"}");
-
-            pw.print(gson.toJson(model.getFromList()));
+                        String name = jobj.get("name").getAsString();
+                        String surname = jobj.get("surname").getAsString();
+                        double salary = jobj.get("salary").getAsDouble();
+                        User user = new User(name, surname, salary);
+                        model.put(input, user);
+                        pw.print("{\"message:\" \"Пользователь успешно изменен\"}");
+                        pw.print(gson.toJson(model.getFromList()));  // Добавлено для наглядности, что удаляются
+                        return;
+                    }
+                }
+                pw.print("{\"message:\" \"No such user with ID:" + request.getParameter("id") + "\"}");
+            }
+        } catch (Exception e) {
+            System.out.println("Error");
         }
     }
 }
